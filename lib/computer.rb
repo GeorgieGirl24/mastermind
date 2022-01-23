684de46ddbea8dc5164fed39f7fd627833df83a4
Kristina Soderholm <gsoderholm@stripe.com>
2022-01-23 14:29:36 -0700

    Add method pattern_maker to created the computer pattern

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,11 +1,27 @@
+require 'pry'
 class Computer
-  attr_reader :size, :pattern
-  def initialize(size=4)
-    @size = size
+  attr_reader :level, :pattern, :letter_choices, :number_characters
+  def initialize(level='beginner')
+  # binding.pry
+    @level = level
+    @letter_choices = {
+                        'beginner': ['R', 'B', 'G', 'Y'],
+                        'intermediate': ['R', 'B', 'G', 'Y', 'M'],
+                        'advanced': ['R', 'B', 'G', 'Y', 'M', 'C']
+    }
+    @number_characters = {
+                          'beginner': 4,
+                          'intermediate': 6,
+                          'advanced': 8
+    }
     @pattern = pattern_maker
   end
 
   def pattern_maker
-    
+    the_pattern = []
+    until the_pattern.length == @number_characters[level.to_sym] do
+       the_pattern << @letter_choices[level.to_sym].sample
+    end
+    the_pattern
   end
 end
